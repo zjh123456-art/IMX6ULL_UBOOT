24c30d9c5eaaf484e7cddfad15a094cda3c06162
zjh_ziji <1103162130@qq.com>
2021-03-15 08:07:12 -0700

    完好的版本1

---
+++ build_uboot_zjh.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "清理工程"
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
+echo "配置uboot"
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- mx6ull_zjh_emmc_defconfig
+echo "编译uboot"
+read -p "精简信息“0” or 详细信息“1”:"  cmd
+make V=${cmd}  ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j16
